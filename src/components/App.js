ff0d92c2ae4bb14f02f2bd1e7bf2040478bb69e9
Sandesh Magdum <sandesh.s_magdum@yahoo.co.in>
2019-06-21 16:54:44 +0530

    Updated component references to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,30 @@
 import React from "react";
 import CommentDetail from "./comments/CommentDetail";
+import GeoLocation from "./geolocation/GeoLocation";
 
-const App = () => {
-  return (
-    <div>
-      <CommentDetail
-        name="Sandy"
-        timeAgo="2 days ago"
-        comment="How artistic!"
-      />
-      <CommentDetail
-        name="Jenny"
-        timeAgo="Just now"
-        comment="Elliot you are always so right :)"
-      />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    console.log("Inside App constructor");
+    super(props);
+  }
+  render() {
+    console.log("Inside App render");
+    return (
+      <div>
+        {/* <CommentDetail
+          name="Sandy"
+          timeAgo="2 days ago"
+          comment="How artistic!"
+        /> */}
+        <CommentDetail
+          name="Jenny"
+          timeAgo="Just now"
+          comment="Elliot you are always so right :)"
+        />
+        <GeoLocation />
+      </div>
+    );
+  }
+}
 
 export default App;
