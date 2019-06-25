02018205c89216ab8917f9fbfb6e112e2863c743
sandesh2782 <sandeshma@cybage.com>
2019-06-25 11:17:42 +0530

    Added search image feature

--- src/components/imagelist/ImageItem.js
+++ src/components/imagelist/ImageItem.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { HREF_LINK } from "../../constants";
 
@@ -21,27 +20,3 @@ export default class ImageItem extends Component {
     );
   }
 }
-=======
-import React, { Component } from "react";
-import { HREF_LINK } from "../../constants";
-
-export default class ImageItem extends Component {
-  render() {
-    return (
-      <div className="item">
-        <a href={HREF_LINK} className="ui tiny image">
-          <img alt="" src={this.props.photo.src.small} />
-        </a>
-        <div className="content">
-          <a href={HREF_LINK} className="header">
-            {this.props.photo.photographer}
-          </a>
-          <div className="description">
-            <p>{this.props.photo.id}</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
->>>>>>> 3873952ff84ed389af51cc2ad37060b888572b38
--- src/components/imagelist/ImageList.js
+++ src/components/imagelist/ImageList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import ImageItem from "./ImageItem";
 
@@ -14,20 +13,3 @@ export default class ImageList extends Component {
     return <div className="ui items">{this.showImages()}</div>;
   }
 }
-=======
-import React, { Component } from "react";
-import ImageItem from "./ImageItem";
-
-export default class ImageList extends Component {
-  showImages = () => {
-    const imageItems = this.props.photos.map(photo => {
-      return <ImageItem photo={photo} key={photo.id} />;
-    });
-    return imageItems;
-  };
-
-  render() {
-    return <div className="ui items">{this.showImages()}</div>;
-  }
-}
->>>>>>> 3873952ff84ed389af51cc2ad37060b888572b38
